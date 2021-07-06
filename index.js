6c4bea2c585084ee7c3e89ab09a255d18c2284f1
Eduardo Ramos <eduardo.auramos@gmail.com>
2021-07-06 20:14:15 -0300

    chore: config eslint and prettier, also initial structure

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-/**
- * @format
- */
-
 import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+import App from './src/App';
+AppRegistry.registerComponent('KompaSaude', () => App);
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { Text, View } from 'react-native';
+
+const App = () => (
+  <View
+    style={{
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#fff',
+    }}>
+    <Text>KompaSaude</Text>
+  </View>
+);
+
+export default App;
